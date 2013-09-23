01922929089b4f0a3ff8404f08b46fb9faaf7ab2
dmitrypodkuiko <dpodkuik@gmail.com>
2013-09-22 20:29:52 -0600

    func struct example

---
+++ chapter1/func.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <thread>
+
+struct func
+{
+    int& i;
+    
+    func(int& i_):i(i_){}
+
+    void operator() ()
+    {
+        for(unsigned j=0;j<1000000;++j)
+        {
+            do_something(i);
+        }
+    }
+};
+
+void oops()
+{
+    int some_local_state=0;
+    func my_func(some_local_state);
+    std::thread my_thread(my_func);
+    my_thread.detach();
+}
+
+void f()
+{
+    int some_local_state=0;
+    func my_func(some_local_state);
+    std::thread t(my_func);
+    try
+    {
+        do_something_in_current_thread();
+    }
+    catch(...)
+    {
+        t.join();
+        throw;
+    }
+    t.join();}
+    
+int main()
+{
+	std::threa
+    d t([]{hello();});
+	t.join();
+	return 0;
+}
